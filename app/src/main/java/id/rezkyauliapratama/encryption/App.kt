46f95cd88acd69256ffd2c54ab9f0647e9781cea
Rezky Aulia Pratama <rezky.pratama@dana.id>
2019-11-30 04:40:11 +0700

    Create app class

---
+++ app/src/main/java/id/rezkyauliapratama/encryption/App.kt
@@ -0,0 +1,11 @@
+package id.rezkyauliapratama.encryption
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        
+    }
+}
