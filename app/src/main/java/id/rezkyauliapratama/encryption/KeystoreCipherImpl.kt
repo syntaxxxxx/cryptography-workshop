b7443a58a306593436c09ee87e60180346ffd30e
Rezky Aulia Pratama <rezky.pratama@dana.id>
2019-11-30 03:29:39 +0700

    Initialize keypair

--- app/src/main/java/id/rezkyauliapratama/encryption/KeystoreCipherImpl.kt
+++ app/src/main/java/id/rezkyauliapratama/encryption/KeystoreCipherImpl.kt
@@ -1,5 +1,22 @@
 package id.rezkyauliapratama.encryption
 
-class KeystoreCipherImpl {
-    
+import android.content.Context
+import java.security.KeyPair
+
+class KeystoreCipherImpl(private val context: Context) {
+
+    //KeyPair that contains Public key and Private key
+    private var masterKeyAsymmetric: KeyPair?
+
+    val keystoreWrapper: KeystoreWrapper by lazy {
+        KeystoreWrapper(context)
+    }
+
+    init {
+        if (!keystoreWrapper.isAndroidKeyStoreAsymmetricKeyExist()) {
+            keystoreWrapper.createAndroidKeyStoreAsymmetricKey()
+        }
+
+        masterKeyAsymmetric = keystoreWrapper.androidKeyStoreAsymmetricKeyPair()
+    }
 }
