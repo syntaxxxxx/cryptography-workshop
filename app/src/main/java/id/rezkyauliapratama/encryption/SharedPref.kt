16a10c447843fc864fce842b0377729a45c81434
Rezky Aulia Pratama <rezky.pratama@dana.id>
2019-11-30 04:30:41 +0700

    Implement capability to store aes in sharedPref

--- app/src/main/java/id/rezkyauliapratama/encryption/SharedPref.kt
+++ app/src/main/java/id/rezkyauliapratama/encryption/SharedPref.kt
@@ -2,11 +2,42 @@ package id.rezkyauliapratama.encryption
 
 import android.content.SharedPreferences
 
-class SharedPref (
-    private val sharedPreferences: SharedPreferences
-){
+class SharedPref(
+    private val sharedPreferences: SharedPreferences,
+    private val cipher: KeystoreCipherImpl
+) {
 
-    
+    companion object {
+        private const val SECURED_PREFS_NAME = "securedSharedPref"
+
+        private const val SECURED_PREFIX = "cipher_"
+        private const val PREF_AES_KEY = SECURED_PREFIX + "securedKey01"
+        private const val PREF_IV_SPEC = SECURED_PREFIX + "securedKey02"
+    }
+
+    init {
+        if (aesKey.isEmpty() || ivSpec.isEmpty()) {
+            createNewAesKey()
+        } else {
+            cipher.setAesKey(aesKey, ivSpec)
+        }
+    }
+
+    private fun createNewAesKey() {
+        val keyPair = cipher.createAesKey()
+        aesKey = keyPair.first
+        ivSpec = keyPair.second
+    }
+
+    private var aesKey: String
+        set(value) = sharedPreferences.put(PREF_AES_KEY, value)
+        get() = sharedPreferences.get(PREF_AES_KEY, String::class.java)
+
+    private var ivSpec: String
+        set(value) = sharedPreferences.put(PREF_IV_SPEC, value)
+        get() = sharedPreferences.get(PREF_IV_SPEC, String::class.java)
+
+
     @Suppress("UNCHECKED_CAST", "IMPLICIT_CAST_TO_ANY")
     protected fun <T> SharedPreferences.get(key: String, clazz: Class<T>): T =
         when (clazz) {
